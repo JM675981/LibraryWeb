d0a3d41edf2167bbf548bbd4218288e94078d3b5
Joshua McCue <jm675981@southeast.edu>
2023-04-15 23:45:24 -0500

    Put together database Models

---
+++ Models/Book.cs
@@ -0,0 +1,27 @@
+﻿using LibraryWeb.Models;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace LibraryWeb.Models
+{
+    public class Book
+    {
+        [Key]
+        public int BookID { get; set; }
+        [Required(ErrorMessage = "Please input a book title")]
+        [DisplayName("Title")]
+        public string BookTitle { get; set; }
+        [DisplayName("Description")]
+        public string? BookDescription { get; set; }
+        [DisplayName("# of Copies")]
+        public int? Count { get; set; }
+
+        //Should be more automated (UserID and data automatically applied)
+        [DisplayName("Added By")]
+        public int AddedID { get; set; }
+        [DisplayName("Edited By")]
+        public int? lastEditID { get; set; }
+        [DisplayName("Last Edited")]
+        public DateTime editDate { get; set; }
+    }
+}
---
+++ Models/Loan.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace LibraryWeb.Models
+{
+    public class Loaned
+    {
+        [Key]
+        public int LoanID { get; set; }
+        [Required(ErrorMessage = "Please select a book")]
+        [DisplayName("Book")]
+        public int BookID { get; set; }
+        public Book Book { get; set; }
+        [Required(ErrorMessage = "Please select a user")]
+        [DisplayName("User")]
+        public int UserID { get; set; }
+
+        //Some automation with some manual input
+        [Required(ErrorMessage = "Please enter the date loaned")]
+        [DisplayName("Date Loaned")]
+        public DateTime DateLoaned { get; set; }
+        [Required(ErrorMessage = "Please enter the date due")]
+        [DisplayName("Due Date")]
+        public DateTime DueDate { get; set; }
+
+        //Might end up automated
+        [Required(ErrorMessage = "Please clarify if the book is still loaned")]
+        [DisplayName("Loaned")]
+        public bool IsLoaned { get; set; }
+    }
+}
