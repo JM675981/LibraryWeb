bb96bf55a68771623c6b1c87988541574f71c73f
Joshua McCue <jm675981@southeast.edu>
2023-04-17 11:30:05 -0500

    Adjusted spacing of elements

--- Models/Loan.cs
+++ Models/Loan.cs
@@ -10,16 +10,17 @@ namespace LibraryWeb.Models
         [Required(ErrorMessage = "Please select a book")]
         [DisplayName("Book")]
         public int BookID { get; set; }
-        public Book Book { get; set; }
+        public Book? Book { get; set; }
         [Required(ErrorMessage = "Please select a user")]
         [DisplayName("User")]
         public string Username { get; set; }
 
         //Some automation with some manual input
         [DisplayName("Date Loaned")]
-        public DateTime DateLoaned { get; set; }
+        public DateTime? DateLoaned { get; set; }
+        [Required(ErrorMessage = "Please set a due date")]
         [DisplayName("Due Date")]
-        public DateTime DueDate { get; set; }
+        public DateTime? DueDate { get; set; }
 
         [DisplayName("Loaned")]
         public bool IsLoaned { get; set; }
